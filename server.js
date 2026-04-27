258bf1929bcae69596d48c671e166c397076e9c9
tri-bis <trisha.biswas.393@gmail.com>
2026-04-27 12:02:42 +0530

    Initial commit: complete project

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Student = require('./models/Student'); 
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/student-management')
+    .then(() => console.log("Database connected successfully"))
+    .catch(err => console.error("Database connection error:", err));
+
+app.get('/', async (req, res) => {
+    try {
+        const students = await Student.find();
+        res.render('index', { students });
+    } catch (err) {
+        res.status(500).send("Error fetching students");
+    }
+});
+
+app.get('/add', (req, res) => {
+    res.render('add-student');
+});
+
+
+app.post('/add-student', async (req, res) => {
+    try {
+        const { name, email, course } = req.body;
+        const newStudent = new Student({ name, email, course });
+        await newStudent.save();
+        res.redirect('/'); 
+    } catch (err) {
+        res.status(400).send("Error saving student: " );
+    }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+});
